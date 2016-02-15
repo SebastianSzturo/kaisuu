addadb828847c99588c8a0ba0e9f51f4e2f10d82
Sebastian Szturo <s.szturo@me.com>
2016-02-15 17:33:34 +0100

    Remove incoming channel messages

--- web/channels/kanji_channel.ex
+++ web/channels/kanji_channel.ex
@@ -8,11 +8,6 @@ defmodule Kaisuu.KanjiChannel do
     {:error, %{reason: "unauthorized"}}
   end
 
-  def handle_in("new_kanji", %{"body" => body}, socket) do
-    broadcast! socket, "new_kanji", %{body: body}
-    {:noreply, socket}
-  end
-
   def handle_out("new_kanji", payload, socket) do
     push socket, "new_kanji", payload
     {:noreply, socket}
